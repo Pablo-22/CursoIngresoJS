6be368d334f2d6274b6ccabe71e6e818879173f1
Pablo-22 <pablocordoba14u@gmail.com>
2021-01-31 00:35:58 -0300

    Se quitó la cadena vacía en el prompt, porque era innecesaria

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,7 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombre = prompt("Por favor, ingrese su nombre", "");
+	let nombre = prompt("Por favor, ingrese su nombre");
     alert("Su nombre es " + nombre);
     
 }
