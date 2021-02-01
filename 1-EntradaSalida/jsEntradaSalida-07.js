a6848e6693c77600a55a7c70a087a2f806c33ec3
Pablo-22 <pablocordoba14u@gmail.com>
2021-02-01 11:42:21 -0300

    Ejercicio 7 resuelto, sujeto a revisiones

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,44 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let numero1;
+    let numero2;
+    
+    numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+    numero2= parseInt(document.getElementById("txtIdNumeroDos").value);
+    
+    let suma = numero1 + numero2;
+    
+    alert(suma);
+    
 }
 
 function restar()
 {
-	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+    numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+    
+    let resta = numero1 - numero2;
+    
+    alert(resta);
 }
 
 function multiplicar()
 { 
-	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+    numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+    
+    let multiplicacion = numero1 * numero2;
+    alert(multiplicacion);
 }
 
 function dividir()
 {
-	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+    numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+    
+    let division = numero1 / numero2;
+
+    alert(division);
 }
 
